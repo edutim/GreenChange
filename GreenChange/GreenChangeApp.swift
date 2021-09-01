ccde128e529fdcdb78fcf348349bb5a46fb82e41
Tim Hart <tim@9red7.com>
2021-08-31 21:35:06 -0400

    Initial Commit

---
+++ GreenChange/GreenChangeApp.swift
@@ -0,0 +1,17 @@
+//
+//  GreenChangeApp.swift
+//  GreenChange
+//
+//  Created by Timothy Hart on 2021-08-31.
+//
+
+import SwiftUI
+
+@main
+struct GreenChangeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
